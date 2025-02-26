09b6f5dfbb815f19c9dd6eca3d0b04c5f39a122b
LouWasHere <louisjoseph003@gmail.com>
2025-02-26 15:16:01 +0000

    cleaning

--- imu_init.py
+++
@@ -1,8 +0,0 @@
-import board
-import busio
-
-i2c = busio.I2C(board.SCL, board.SDA)
-print("I2C initialized:", i2c.try_lock())
-devices = i2c.scan()
-i2c.unlock()
-print("I2C devices found:", devices)
