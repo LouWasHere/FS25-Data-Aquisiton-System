82bda4a41d93e9331040164d0efce98aad4127af
LouWasHere <louisjoseph003@gmail.com>
2025-06-27 15:48:21 +0100

    new script

--- startSystem.sh
+++ startSystem.sh
@@ -1,9 +1,9 @@
-sleep 5
-
 sudo dhclient -v usb0
 sudo udhcpc -i usb0
 sudo route add -net 0.0.0.0 usb0
 
 ngrok tcp 22 &
 sleep 5
-python3 Documents/message_Qt.py
+kill -9 "$(pgrep ngrok)"
+sleep 1
+python3 Documents/server.py &
